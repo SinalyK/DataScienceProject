c1d4e59540fff0f73713399a73d4752504cbc962
SinalyK <sinaly.kanadjigui.23@ump.ac.ma>
2025-09-20 10:55:08 +0100

    Data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src import logger
 from src.pipeline.data_ingestion_pipeline import DataIngestionTariningPipeline
 from src.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 
 logger.info("Welcome to our Custom logger")
@@ -21,6 +22,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
